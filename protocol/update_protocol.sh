f4888ebfb4226a6f116e4d3878e262f8800395c2
zjor <zjor.se@gmail.com>
2012-01-18 15:36:25 +0400

    protocol was added

---
+++ protocol/update_protocol.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+MAIN_PROJECT_PATH=../mobile-dispatcher/src/main/java/
+TEST_PROJECT_PATH=../dispatcher-test/src/main/java/
+
+protoc --java_out=$MAIN_PROJECT_PATH news.proto
+protoc --java_out=$MAIN_PROJECT_PATH commons.proto
+protoc --java_out=$MAIN_PROJECT_PATH currency.proto
+protoc --java_out=$MAIN_PROJECT_PATH banners.proto
+protoc --java_out=$MAIN_PROJECT_PATH locations.proto
+protoc --java_out=$MAIN_PROJECT_PATH partners.proto
+protoc --java_out=$MAIN_PROJECT_PATH protocol.proto
+
+protoc --java_out=$TEST_PROJECT_PATH news.proto
+protoc --java_out=$TEST_PROJECT_PATH commons.proto
+protoc --java_out=$TEST_PROJECT_PATH currency.proto
+protoc --java_out=$TEST_PROJECT_PATH banners.proto
+protoc --java_out=$TEST_PROJECT_PATH locations.proto
+protoc --java_out=$TEST_PROJECT_PATH partners.proto
+protoc --java_out=$TEST_PROJECT_PATH protocol.proto
